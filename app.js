4b7f36496852fc78707a929ca5108964a8bf9d52
jinsuSang <climbplant39@kakao.com>
2021-08-10 20:58:47 +0900

    feature: use validator module

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const fs = require('fs')
 const add = require('./utils')
 const getNotes = require('./notes')
+const validator = require('validator').default
 
 fs.writeFileSync('notes.txt', 'This file was created by Node.js\n')
 fs.appendFileSync('notes.txt', 'My name is jinsu\n')
 console.log(add(5, 6))
 console.log(getNotes())
+
+console.log(validator.isEmail('A@kakao.com'))
+console.log(validator.isURL('https://www.ssafy.com/'))
