773fbfcb8493519ca6028074d061f034eb7144d9
jinsuSang <climbplant39@kakao.com>
2021-08-13 08:59:34 +0900

    feature: printing in color using chalk module

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const add = require('./utils')
 const getNotes = require('./notes')
 const validator = require('validator').default
+const chalk = require('chalk')
 
 fs.writeFileSync('notes.txt', 'This file was created by Node.js\n')
 fs.appendFileSync('notes.txt', 'My name is jinsu\n')
@@ -10,3 +11,5 @@ console.log(getNotes())
 
 console.log(validator.isEmail('A@kakao.com'))
 console.log(validator.isURL('https://www.ssafy.com/'))
+console.log(chalk.blue('hello'))
+console.log(chalk.red.bgBlack`2 + 3 = {bold ${2 + 3}}`)
