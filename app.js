b2a841d8b921f7386047379cb1faa44f8415c742
jinsuSang <climbplant39@kakao.com>
2021-08-13 11:06:02 +0900

    feature: get input from user by using process.argv

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 const notes = require('./notes')
 const validator = require('validator').default
 const chalk = require('chalk')
+
+const command = process.argv[2]
+
+if (command === 'add') {
+  console.log('add')
+} else if (command === 'remove') {
+  console.log('remove note')
+}
