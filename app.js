70b8e9afbcdb58c947f5084d63d89cf402671444
jinsuSang <climbplant39@kakao.com>
2021-08-15 22:38:25 +0900

    feat: arguments parsing with Yargs

--- app.js
+++ app.js
@@ -1,11 +1,53 @@
-const notes = require('./notes')
-const validator = require('validator').default
-const chalk = require('chalk')
+'use strict'
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
 
-const command = process.argv[2]
-
-if (command === 'add') {
-  console.log('add')
-} else if (command === 'remove') {
-  console.log('remove note')
-}
+yargs(hideBin(process.argv))
+  .command(
+    'add',
+    'Add a note',
+    () => {},
+    (argv) => {
+      console.log(`Add a note with the title "${argv.title}"...`)
+      console.log(`What is written is below.\n"${argv.body}"`)
+    }
+  )
+  .demandOption(['title', 'body'])
+  .command(
+    'remove',
+    'Remove the note',
+    () => {},
+    (argv) => {
+      console.log(`Remove a note with the title "${argv.title}"...`)
+    }
+  )
+  .demandOption(['title'])
+  .command(
+    'list',
+    'List notes',
+    () => {},
+    () => {
+      console.log(`List notes...`)
+    }
+  )
+  .command(
+    'read',
+    'Read a note',
+    () => {},
+    (argv) => {
+      console.log(`Read a note with the title "${argv.title}"...`)
+    }
+  )
+  .demandOption(['title'])
+  .options({
+    title: {
+      alias: 't',
+      type: 'string',
+      describe: '노트 이름을 입력합니다',
+    },
+    body: {
+      alias: 'b',
+      type: 'string',
+      describe: '노트 내용을 입력합니다',
+    },
+  }).argv
